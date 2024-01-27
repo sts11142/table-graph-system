e4c43df4f071ee9697146bc13785ee4f27e6e6fc
Shota Sato <last11142@gmail.com>
2024-01-27 18:41:56 +0900

    feat: fetch api → papaparse の一連の処理を実装した

--- src/components/LoadCSVButton/hooks.ts
+++ src/components/LoadCSVButton/hooks.ts
@@ -1,15 +1,30 @@
+import { CsvFile } from "@/types/CsvFile";
 import { useEffect, useState } from "react";
+import { usePapaParse } from "react-papaparse";
 
 export function useFetchCsv() {
-  const [file, setFile] = useState([])
+  const [file, setFile] = useState<CsvFile>([]);
+  const { readString } = usePapaParse();
 
   useEffect(() => {
-    const fetchFile = async (url:string) => {
-      const response = await fetch(url)
-      await console.log(response.text())
-    }
-    fetchFile('http://www.mn.cis.iwate-u.ac.jp/~nakaya/report/Data.csv')
-  }, [])
+    // httpリクエストを投げてCSVファイルを取得する関数(top-level async 用の関数宣言)
+    const fetchFile = async (url: string) => {
+      // Promiseのストリームからデータを取得する
+      const csvString = await (await fetch(url)).text();
 
-  return [file]
+      // CSV形式のデータを papaparse ライブラリを用いてArray<object>型に変換する
+      readString(csvString, {
+        worker: true,
+        header: true,  // csvヘッダー有 の処理を行う
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        complete: (result: any) => {
+          setFile(result.data);
+        },
+      });
+    };
+
+    fetchFile("http://www.mn.cis.iwate-u.ac.jp/~nakaya/report/Data.csv");
+  }, [readString]);
+
+  return [file];
 }
