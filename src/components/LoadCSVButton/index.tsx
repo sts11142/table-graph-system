0239c35599890bdb2eddaaa7a2822c25c396b170
Shota Sato <last11142@gmail.com>
2024-01-27 19:34:31 +0900

    delete: 前回の整理に関連して不要なファイルを削除する

--- src/components/LoadCSVButton/index.tsx
+++ src/components/LoadCSVButton/index.tsx
@@ -1,5 +1,2 @@
 /* eslint-disable react-refresh/only-export-components */
 export * from './LoadCSVButton'
-export * from './LoadCSVButtonVar'
-export * from './LoadCSVButtonFile'
-export * from './LoadCSVButtonHttp'
