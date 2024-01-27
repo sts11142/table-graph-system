90c5c150010152e3bbd303f6b3dd9f8726e6b3ec
Shota Sato <last11142@gmail.com>
2024-01-27 22:18:45 +0900

    feat: csvファイルの最終行(空行)がある場合にこれを取り除く処理を追加

--- src/hooks/useFetchCsv.ts
+++ src/hooks/useFetchCsv.ts
@@ -2,6 +2,26 @@ import { CsvFile } from "@/types/CsvFile";
 import { useEffect, useState } from "react";
 import { usePapaParse } from "react-papaparse";
 
+const removeLastIfEmpty = (csvFile: CsvFile): CsvFile => {
+  const newCsv = [...csvFile]
+
+  // 配列が空でないか確認
+  if (newCsv.length === 0) {
+      return newCsv;
+  }
+
+  // 最後の要素を取得
+  const lastElement = newCsv[newCsv.length - 1];
+
+  // csvファイルには欠損値が全くないものと仮定する
+  if (Object.keys(lastElement).length !== 8) {
+      // 最後の要素が空のオブジェクトの場合、それを削除
+      newCsv.pop();
+  }
+
+  return newCsv;
+}
+
 export function useFetchCsv(path: string) {
   const [file, setFile] = useState<CsvFile>([]);
   const { readString } = usePapaParse();
@@ -18,7 +38,7 @@ export function useFetchCsv(path: string) {
         header: true, // csvヘッダー有 の処理を行う
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         complete: (result: any) => {
-          setFile(result.data);
+          setFile(removeLastIfEmpty(result.data));
         },
       });
     };
